88fe579b83f30d3422ee753b73d4fe177403bf3f
abdulrahman <gabdulrahman.mohammed@gmail.com>
2023-12-30 14:32:59 +0200

    remove not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,12 @@ import "./assets/style/custom.css"
 // import 'bootstrap-icons/font/bootstrap-icons.css'
 import "bootstrap/dist/css/bootstrap.css"
 import Tickets from "./Pages/tickets"
-import { Route, Routes } from "react-router-dom"
-import NotFound from "./Pages/NotFound"
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route path='/not-found' element={<NotFound />}/>
-      <Route path='/' element={<Tickets />} />
-    </Routes>
+      <Tickets />
     </>
   )
 }
