4743ef1ad0e19b36d8b81326b0257d8d4a7cc87a
abdulrahman <gabdulrahman.mohammed@gmail.com>
2023-12-30 14:30:27 +0200

    not found

--- src/Pages/tickets.jsx
+++ src/Pages/tickets.jsx
@@ -22,17 +22,22 @@ export default function Tickets() {
         .then(res => {
             setData(res.data.RFNO)
             console.log(res.data.RFNO)
+            console.log(res.data.error)
+            if(res.data.error){
+              setErrorMSG(true)
+            }
             })
         .catch(e => { console.log(e)})
     },[lang])
 
-    console.log(data)
     const options = [
       {value:1 , label:<span><img src={SaudiIcon} alt='SaudiArabia' width={18}/> العربية</span>},
       {value:2 , label:<span><img src={USIcon} alt='US' width={18}/> English</span>}
     ]
+
   return (
     <div className='content-page'>
+      {!errorMSG ? 
       <Container>
         {
             data !== null && data != undefined ?
@@ -42,7 +47,7 @@ export default function Tickets() {
           <div className='text-end'>
             <div className='d-flex justify-content-end mb-2'>
                 <div style={{width: '130px'}}>
-                  <Select styles={{width: '40px'}} defaultValue={options[0]} options={options} onChange={(e)=>{setLang(e.value);console.log(e.value)}}/>
+                  <Select styles={{width: '40px'}} defaultValue={options[0]} options={options} onChange={(e)=>{setLang(e.value)}}/>
                 </div>
             </div>
           </div>
@@ -80,10 +85,23 @@ export default function Tickets() {
         <div className='col-lg-11 col-md-10 col-sm-12 m-auto mb-4'>
             <div className='form-container shadow-lg p-4 bg-body rounded-bottom text-center'>
                 <Spinner />
+                {/* {data == undefined && setTimeout(()=>history.push('/not-found'))} */}
             </div>
         </div>
         }
       </Container>
+      :
+      <Container>
+      <div className='col-lg-11 col-md-10 col-sm-12 m-auto mb-4'>
+          <div className='bg-secondary text-white  p-4 form-title rounded-top fw-bold'>
+              Error 404
+          </div>
+          <div className='form-container shadow-lg p-4 bg-body rounded-bottom d-flex align-items-center text-center justify-content-center' style={{height:'200px'}}>
+              <h4>This page is Not Found</h4>
+          </div>
+      </div>
+      </Container>
+      }
     </div>
   )
 }
